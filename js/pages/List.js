e62861a3b7206bae339c2fedabba254a0719ca3e
LucaGMD <92657248+LucaGoku@users.noreply.github.com>
2025-09-12 18:38:36 -0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -98,30 +98,33 @@ export default {
                             </li>
                         </ol>
                     </template>
-                    <h3>Submission Requirements</h3>
+                    <h3>List Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        The difficulty must be almost all in the spam of the level. You are allowed to put a triple spike or a timing at the end or beginning.
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        You are not allowed to use methods of spamming that require little effort for very high amounts of cps, such as drag clicking, bolt clicking.
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        The lowest respawn time is 0.5 seconds.
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        A maximum of 2 inputs are allowed when spamming.
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        You must not use capped hardware to spam as it gives an unfair advantage at frame perfect spam.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        Rebinding keys IS allowed as long as you use only 2 or less keys!
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        It may say Spam "Challenge" List, however there is not really a time limit.
                     </p>
                     <p>
                         Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                    </p>
+                    <p>
+                        Only levels that have 15 CPS frame perfect spam are allowed on the list. Higher FPS or harder methods are allowed to make the level harder.
                     </p>
                 </div>
             </div>
